422daa6bfe6b18ba458980c6674ba1d929a35ec3
OlexandrFedorkoDev <Fedorko2323@gmail.com>
2021-02-23 13:54:23 +0200

    commit

---
+++ app/js/main.js
@@ -0,0 +1,50 @@
+$(function () {
+    $(".products_slider").slick({
+        prevArrow: '<button class="slider-btn slider-btn_left"> <svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d = "M9.21839 1L1 9L9.21839 17"/></svg></button>',
+        nextArrow: '<button class="slider-btn slider-btn_right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1"/></svg></button>',
+        infinite: false,
+    }
+
+    );
+
+    $(".questions_item-title").on('click', function () {
+        $(".questions_item").removeClass('questions_item-active');
+        $(this).parent().addClass('questions_item-active');
+    }
+
+    );
+
+
+    $('#fullpage').fullpage({
+        autoScrolling: true,
+        scrollHorizontally: true,
+        sectionSelector: '.page-section',
+        scrollOverflow: true,
+        anchors: ['header', 'products', 'benefits', 'specification', 'questions', 'contacts'],
+        menu: '#header_nav'
+    }
+
+    );
+
+
+
+
+    $(".menu_btn").on("click", function () {
+        $(".menu_btn").toggleClass("menu_btn-active");
+        $(".menu_list").toggleClass("menu_list-active");
+    }
+
+    );
+
+    $(".menu_list-link").on("click", function () {
+        $(".menu_btn").removeClass("menu_btn-active");
+        $(".menu_list").removeClass("menu_list-active");
+    }
+
+    );
+
+
+
+}
+
+);
